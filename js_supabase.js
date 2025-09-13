cc26473322bbc7c630b09885191f1e6890dc7499
Ramu1023 <priyareddy1023123@gmail.com>
2025-09-13 08:25:45 +0530

    Update js_supabase.js
    me

--- js_supabase.js
+++ js_supabase.js
@@ -1,6 +1,6 @@
 //js&supabase.js
-const SUPABASE_URL = 'https://qdmlbedacymowcgtykvm.supabase.co'; // YOUR_SUPABASE_URL
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkbWxiZWRhY3ltb3djZ3R5a3ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyNjMwOTUsImV4cCI6MjA2NDgzOTA5NX0.oQ98WG4hXtlwTUaj5Aj9R6UVHPZvgR0iQnhxuVtVfRc'; // YOUR_SUPABASE_ANON_KEY
+const SUPABASE_URL = 'https://rnjvqxdrvplgilqzwnpl.supabase.co'; // YOUR_SUPABASE_URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuanZxeGRydnBsZ2lscXp3bnBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5NDU4NjYsImV4cCI6MjA2NDUyMTg2Nn0.IOAxp8ULZgccX8hKtlDQzwdrW7xp1CcXVSdJ59UEruA'; // YOUR_SUPABASE_ANON_KEY
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY || SUPABASE_URL.includes('YOUR_SUPABASE_URL')) {
     alert("Application is not configured correctly. Supabase credentials missing in js/js_supabase.js");
